e01f81526875e071d2b195882588b5befc4f9187
Vignesh Kumar <dvigneshkumar3@gmail.com>
2021-08-01 22:56:14 +0530

    tree potten da

---
+++ bst.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* left;
+
+    struct node* right;
+};
+
+struct node* newnode(int x){
+   struct node* nn=(struct node*)malloc(sizeof(struct node));
+
+   nn->data=x;
+   nn->left=nn->right=NULL;
+   return nn;
+
+}
+
+struct node* insert(struct node* root,int x){
+    
+    struct node* nn=newnode(x);
+    
+    if(root==NULL){
+        root=nn;
+    }
+   else{
+       struct node* temp=root;
+       if(x<temp->data)
+            temp->left=insert(temp->left,x);
+    else if(x>temp->data)
+            temp->right=insert(temp->right,x);
+   }
+   return root;
+}
+int height(struct node* root){
+    if(root==NULL)
+        return 0;
+    int lh=height(root->left);
+    int rh=height(root->right);
+    if(rh>lh)
+        return rh+1;
+    else
+        return lh+1;
+}
+
+
+void levelorder(struct node* root,int level){
+    if(root==NULL)
+    return;
+    if(level==0)
+        printf("%d ",root->data);
+        if(level>0){
+    levelorder(root->left,level-1);
+    levelorder(root->right,level-1);
+    }
+}
+
+
+void callevelorder(struct node* root){
+
+int h=height(root);
+int i;
+for(i=0;i<h;i++){
+    levelorder(root,i);
+    }
+}
+
+void inorder(struct node* root){
+    if(root!=NULL){
+        inorder(root->left);
+        printf("%d ",root->data);
+        inorder(root->right);
+    }
+}
+
+
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    struct node* root=NULL;
+    int i;
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        root=insert(root,arr[i]);
+    }
+    // inorder(root);
+    callevelorder(root);
+
+}
---
+++ subsetsum.c
@@ -0,0 +1,33 @@
+//display true or false if a subset with that sum is present
+
+//recursion exponential 2^n TC
+
+#include<stdio.h>
+int subset(int arr[],int n,int sum){
+    if(sum==0)
+        return 1;
+    if(sum!=0&&n==0)
+        return 0;
+    if(arr[n-1]>sum)
+        return subset(arr,n-1,sum);
+
+    return subset(arr,n-1,sum)|| subset(arr,n-1,sum-arr[n-1]);
+
+}
+
+int main(){
+    int n;
+    scanf("%d,&n");
+    int arr[n];
+    int i;
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int sum;
+    printf("sum\n:");
+    scanf("%d",&sum);
+    if(subset(arr,n,sum)== 1)
+        printf("subsset true");
+    else
+        printf("subset fals:");
+}
