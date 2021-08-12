8367823fd8888a100ce14fd996ec2ae6b8d178f6
kaiser-x <tk45935@gmail>
2021-08-12 22:23:11 +0530

    few new sums

--- Fun/CountTrailingZeros.c
+++ Fun/CountTrailingZeros.c
@@ -35,7 +35,7 @@ int main(){
     int n;
     scanf("%d",&n);
     int count=0;
-    for(int i=0;n/i>=1;i=i*5){
+    for(int i=5;n/i>=1;i=i*5){
         count+=n/i;
     }
 
--- hello.c
+++ hello.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
+
+int bits_cal(int n){
+    int count=0;
+		while(n>0) {
+			n=n&(n-1);
+			count++;
+		}
+        return count;
+}
 int main(){
-    printf("helloworld");
-    int age;
-    scanf("%d",&age);
-    printf("\n%d",age);
+    // printf("helloworld");
+    // int age;
+    // scanf("%d",&age);
+    // printf("\n%d",age);
+    int n
     return 0;
 }
